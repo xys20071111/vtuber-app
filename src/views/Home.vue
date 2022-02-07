439ec5a6a3ffea5e663b4a6c78a42bd1a400ebd2
xys20071111 <xys20071111@hotmail.com>
2022-02-07 16:53:36 +0800

    添加用于重新加载模型的接口

--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,13 +1,25 @@
 <template>
   <div class="home">
-    <img alt="Vue logo" src="../assets/logo.png">
+    <img alt="Vue logo" src="../assets/logo.png" />
+    <el-button @click="test">测试</el-button>
   </div>
 </template>
 
 <script lang="ts">
 import { defineComponent } from 'vue'
+import { ElButton } from 'element-plus'
 
 export default defineComponent({
-  name: 'Home'
+  name: 'Home',
+  components: {
+    ElButton
+  },
+  setup () {
+    const test = () => {
+      window.control.reloadModel()
+    }
+
+    return { test }
+  }
 })
 </script>
